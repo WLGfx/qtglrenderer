ba428b0a4788e12b408b62564edb88f6da7dd38e
WLGfx <csnorwood72@gmail.com>
2016-04-15 12:09:50 +0100

    updated main.cpp

--- cwengage3/main.cpp
+++ cwengage3/main.cpp
@@ -1,11 +1,17 @@
-#include "window.h"
+#include "global.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
 	Window w;
 	w.show();
+	Global::window = &w;
+
+	Global::udp_thread = new UdpClientThread();
+	Global::tcp_thread = new TcpClientThread();
+	Global::manager = new EventManager();
 
 	return a.exec();
 }
